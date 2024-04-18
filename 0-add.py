81deb9697a14e40f75c3d72ec0f62e1d8a768085
ehabsmh <ehabsmh3@gmail.com>
2024-04-18 23:36:42 +0200

    file is executable

--- 0-add.py
+++ 0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """0. Basic annotations - add"""
 
-def add(a: float, b: float) -> float:
-    """takes a float @a and a float @b as arguments and returns their sum as a float."""
-    return a + b
 
+def add(a: float, b: float) -> float:
+    """takes a float @a and a float @b as arguments and
+    returns their sum as a float."""
+    return a + b
