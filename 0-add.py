25f9b53c50e90fa333531737ac61aa9400c01bc7
ehabsmh <ehabsmh3@gmail.com>
2024-04-18 23:32:46 +0200

    0. Basic annotations - add

---
+++ 0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""0. Basic annotations - add"""
+
+def add(a: float, b: float) -> float:
+    """takes a float @a and a float @b as arguments and returns their sum as a float."""
+    return a + b
+
